1c639882e859e180ef2c95185f3a6de8d80c4cf0
sangramabdar <abdarsangram2697@gmail.com>
2024-01-14 16:26:34 +0530

    added new script in package.json

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import appConfig from "./config/app.config";
 import app from "./config/express.config";
 
+console.log(appConfig);
+
 app.listen(appConfig.PORT, (): void => {
   console.log("SERVER IS UP ON PORT:", appConfig.PORT);
 });
